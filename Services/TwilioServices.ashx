2a88ac55c7ea35554328fb784918358de61fd93c
Sebastian Gnagnarella <sebasg@gmail.com>
2012-12-26 12:56:22 -0700

    Initial commit test

---
+++ Services/TwilioServices.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="TwilioServices.ashx.cs" Class="Services.TwilioServices" %>
---
+++ Services/TwilioServices.ashx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Services
+{
+    /// <summary>
+    /// Summary description for TwilioServices
+    /// </summary>
+    public class TwilioServices : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Clear(); 
+            context.Response.ContentType = "text/xml"; 
+            context.Response.ContentEncoding = System.Text.Encoding.UTF8; 
+            string twiMLResponse = "<Response><Say>Hello World</Say></Response>"; 
+            context.Response.Write(twiMLResponse); 
+            context.Response.End(); 
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
