d46e91aee9ee700536c7744f09ad5c41ba2d6e1a
sgnagnarella <sebasg@gmail.com>
2013-04-15 15:01:01 -0600

    Changes for Call forwarding

--- Services/Call.ashx.cs
+++ Services/Call.ashx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Twilio.TwiML;
+using Twilio;
 
 namespace Services
 {
@@ -14,14 +15,23 @@ namespace Services
 
         public void ProcessRequest(HttpContext context)
         {
-            context.Response.Clear(); 
-            context.Response.ContentType = "text/xml"; 
+            context.Response.Clear();
+            context.Response.ContentType = "text/xml";
             context.Response.ContentEncoding = System.Text.Encoding.UTF8;
             var twiml = new Twilio.TwiML.TwilioResponse();
-            twiml.Say("Hello Seba");
-            twiml.Enqueue("QueueDemo");
-            context.Response.Write(twiml.ToString()); 
+
+            //twiml.Say("Hello Seba");
+            //twiml.Enqueue("QueueDemo");
+            twiml.Dial("+17202822742");
+            context.Response.Write(twiml.ToString());
             context.Response.End(); 
+            //string accountSid = "AC910947335f85f6b222394231ac5f064d";
+            //string authToken = "44e9b67a8f82209656db45e7575759c5";
+            //var twilio = new TwilioRestClient(accountSid, authToken);
+            //var call = twilio.InitiateOutboundCall("+17209615246", "+17202822742", "http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient");
+
+
+
         }
 
         public bool IsReusable
